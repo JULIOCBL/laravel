f679c8832a55435424d5c73a0c2028564f7834c1
JULIOCBL <julio-borgeslopez@outlook.com>
2022-04-10 23:23:35 -0500

    compilado modo produccion

--- public/assets/js/slider.js
+++ public/assets/js/slider.js
@@ -1,10 +1,16 @@
+/******/ (() => { // webpackBootstrap
+var __webpack_exports__ = {};
+/*!********************************!*\
+  !*** ./resources/js/slider.js ***!
+  \********************************/
 $(document).ready(function () {
-    $("#autoWidth").lightSlider({
-        autoWidth: true,
-        loop: true,
-        onSliderLoad: function () {
-            $("#autoWidth").removeClass("cS-hidden");
-        },
-    });
+  $("#autoWidth").lightSlider({
+    autoWidth: true,
+    loop: true,
+    onSliderLoad: function onSliderLoad() {
+      $("#autoWidth").removeClass("cS-hidden");
+    }
+  });
 });
-
+/******/ })()
+;
